84f5fc9a73b0e6478f984504fa06145db8853df9
Colin Nico Schmid <mail@mala23.me>
2018-12-18 13:12:27 +0100

    create caffeine section

--- src/components/section/index.js
+++ src/components/section/index.js
@@ -5,7 +5,16 @@ import styles from './style.css'
 
 const Section = (props) => (
   <section>
-    <h2>Section</h2>
+    <h2>Koffein – Wirkung auf Adenosinrezeptoren</h2>
+    <span></span>
+    <p class="section-intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    <figure class="cycle">
+    </figure>
+    <figure class="atmospheric">
+    </figure>
+    <figure class="illustration">
+      <img class="cycle-image" src="https://s3.eu-central-1.amazonaws.com/healthysleep/illustrations/caffeine_synapse.png" alt="Illustration: Adenosinrezeptoren einer Synapse"/>
+    </figure>
   </section>
 )
 
